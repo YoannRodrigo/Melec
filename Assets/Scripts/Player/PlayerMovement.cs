cd27f1806b1d9c1b302f224b02b7956244ae1783
Yoann <yomaisonneuve@gmail.com>
2020-11-13 20:07:17 +0100

    PlayerMovement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+   private const float SPEED = 3;
+   private Rigidbody rb;
+   
+   private void Start()
+   {
+      rb = GetComponent<Rigidbody>();
+   }
+
+   private void Update()
+   {
+      MovePlayer();
+   }
+
+   private void MovePlayer()
+   {
+      Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+      rb.transform.position += movement * (SPEED * Time.deltaTime);
+   }   
+}
