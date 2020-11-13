b8c007225b47330688e577811c71e9634b0f10ca
HxM <anthony.cuttivet@gmail.com>
2020-11-13 23:05:01 +0100

    Aled

--- Assets/Scripts/AnthoNePasOuvrir/testManager.cs
+++ Assets/Scripts/AnthoNePasOuvrir/testManager.cs
@@ -7,7 +7,7 @@ public class testManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameObject g = AtomsManager._instance.SpawnAtom(AtomsManager.AtomAbb.H, new Vector3(3f,1f,0f));
+        
     }
 
     // Update is called once per frame
