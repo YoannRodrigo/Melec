0c6a60eed7595e85b9347fabebaf92042540d12d
Yoann <yomaisonneuve@gmail.com>
2020-11-14 00:13:20 +0100

    Enemy look at player and shoot
    ToDo : Enemy move, better shoot precision

---
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private Rigidbody playerRigidbody;
+    private float timeSinceLastShot;
+    public GameObject projectilePrefab;
+    public GameObject firePoint;
+    private const float TIME_BEFORE_SHOT = 2;
+
+    private void Start()
+    {
+        playerRigidbody = GameObject.FindWithTag("Player").GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        transform.LookAt(Vector3.Scale(playerRigidbody.velocity + playerRigidbody.position, new Vector3(1, 0, 1)));
+        if (timeSinceLastShot > TIME_BEFORE_SHOT)
+        {
+            timeSinceLastShot = 0;
+            ShootProjectile();
+        }
+        timeSinceLastShot += Time.deltaTime;
+    }
+
+    private void ShootProjectile()
+    {
+        GameObject projectile = Instantiate(projectilePrefab, firePoint.transform.position, Quaternion.identity);
+        projectile.GetComponent<ProjectileCollision>().SetEnemyInstanceId(gameObject.GetInstanceID());
+        projectile.GetComponent<Rigidbody>().AddForce(2*transform.forward,ForceMode.Impulse);
+    }
+}
---
+++ Assets/Scripts/Enemies/ProjectileCollision.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ProjectileCollision : MonoBehaviour
+{
+    private int enemyInstanceId;
+
+    public void SetEnemyInstanceId(int enemyInstanceId)
+    {
+        this.enemyInstanceId = enemyInstanceId;
+    }
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        if(enemyInstanceId != other.gameObject.GetInstanceID())
+        {
+            Destroy(gameObject);
+        }
+    }
+}
