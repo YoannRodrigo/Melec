6e93a74ffaeef6ef14ed6d0b02d8d46bd6527fe5
Yoann <yomaisonneuve@gmail.com>
2020-11-13 21:15:51 +0100

    First Dungeon Generator

---
+++ Assets/Scripts/Room/ScriptableRoom.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Room", menuName = "ScriptableObjects/ScriptableRoom", order = 1)]
+public class ScriptableRoom : ScriptableObject
+{
+    public int nbEnemies;
+    public GameObject roomPrefab;
+
+    public Transform SpawnRoom(Vector3 position)
+    {
+        Vector3 newPosition = new Vector3(position.x,position.y,position.z + 15f);
+        return Instantiate(roomPrefab, newPosition, Quaternion.identity).transform;
+    }
+}
