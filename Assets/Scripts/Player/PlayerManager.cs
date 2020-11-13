ecc8de9f98f32d7fa249a676b61a31978b51f131
Yoann <yomaisonneuve@gmail.com>
2020-11-13 23:33:06 +0100

    Basic Player Life sytem

---
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    private float life = 100;
+
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("BadProjectile"))
+        {
+            life -= 5;
+            Destroy(other.gameObject);
+            CheckPlayerLife();
+        }
+    }
+
+    private void CheckPlayerLife()
+    {
+        if (life <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
