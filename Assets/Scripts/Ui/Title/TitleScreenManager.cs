cab8d044b8bdf11a6618fb2bb369946803a55fba
HxM <anthony.cuttivet@gmail.com>
2020-11-14 23:36:13 +0100

    Title screen

---
+++ Assets/Scripts/Ui/Title/TitleScreenManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TitleScreenManager : MonoBehaviour
+{
+
+    public string selectedMenu;
+
+    public GameObject buttonsGO;
+    public GameObject arrow;
+    
+
+    // Update is called once per frame
+    void Update() {
+        if (selectedMenu == "") {
+            selectedMenu = EventSystem.current.currentSelectedGameObject.name;
+        }else if(EventSystem.current.currentSelectedGameObject.name != selectedMenu) {
+            selectedMenu = EventSystem.current.currentSelectedGameObject.name;
+            OnChangeSelectedGO();
+        }
+    }
+    
+    public void OnChangeSelectedGO(){
+        arrow.transform.localPosition = new Vector3(arrow.transform.localPosition.x,EventSystem.current.currentSelectedGameObject.transform.localPosition.y,arrow.transform.localPosition.z);
+    }
+}
