43619dbffeb0fdb187d5b5c36709a417c166bb4b
HxM <anthony.cuttivet@gmail.com>
2020-11-14 12:23:57 +0100

    Merging system in inventory

--- Assets/Scripts/Collectable/Atom.cs
+++ Assets/Scripts/Collectable/Atom.cs
@@ -1,12 +1,12 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Atom", menuName = "ScriptableObjects/Atom", order = 1)]
+[CreateAssetMenu(fileName = "Atom", menuName = "ScriptableObjects/Collectable/Atom", order = 1)]
 public class Atom : ScriptableObject
 {
-    public string name;
-    public AtomsManager.AtomAbb abbreviation;
+    public string atomName;
+    public CollectablesManager.AtomAbb abbreviation;
     public Sprite sprite;
 
 }
---
+++ Assets/Scripts/Fusions/Molecule.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Molecule", menuName = "ScriptableObjects/Collectable/Molecule", order = 1)]
+public class Molecule : ScriptableObject
+{
+    public string moleculeName;
+    public CollectablesManager.MoleculeAbb abbreviation;
+    public Sprite sprite;
+}
