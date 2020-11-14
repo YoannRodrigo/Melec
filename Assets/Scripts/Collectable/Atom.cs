fd9c178baad4e57e817b969b3a4f4465367b8fe3
Yoann <yomaisonneuve@gmail.com>
2020-11-14 10:29:21 +0100

    Cherry Pick

---
+++ Assets/Scripts/Collectable/Atom.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Atom", menuName = "ScriptableObjects/Atom", order = 1)]
+public class Atom : ScriptableObject
+{
+    public string name;
+    public AtomsManager.AtomAbb abbreviation;
+    public Sprite sprite;
+
+}
---
+++ Assets/Scripts/Player/Inventory.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+    public List<Atom> inventory = new List<Atom>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Add(Atom atomToAdd){
+        print("Added " + atomToAdd + " to Inventory");
+        inventory.Add(atomToAdd);
+    }
+
+/*     public void Remove(Atom atomToRemove){
+        
+    } */
+}
