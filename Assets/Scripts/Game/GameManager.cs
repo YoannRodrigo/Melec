44ac226b1ad1fb846e62b0e2db106bdab4f5845e
Yoann <yomaisonneuve@gmail.com>
2020-11-15 18:19:40 +0100

    Fix laggy merge menu

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -16,8 +16,6 @@ public class GameManager : MonoBehaviour
     public GameStates gameState;
     public GameObject player;
     private Inventory inventory;
-    private const float TIME_BEFORE_CLICK = 1;
-    private float timeSinceLastClick = 1;
 
     private void Awake()
     {
@@ -34,52 +32,45 @@ public class GameManager : MonoBehaviour
 
     // Update is called once per frame
     private void Update() {
-        timeSinceLastClick += Time.deltaTime;
-        
-            ToggleMergerUI();
+        ToggleMergerUI();
 
     }
 
     private void ToggleMergerUI()
     {
-        if (Input.GetButton("InventoryMode") && SceneManager.GetActiveScene().buildIndex != 0 && SceneManager.GetActiveScene().buildIndex != 1)
+        if (Input.GetKeyDown(KeyCode.Space) && SceneManager.GetActiveScene().buildIndex != 0 && SceneManager.GetActiveScene().buildIndex != 1)
         {
-            if (timeSinceLastClick > TIME_BEFORE_CLICK)
+            switch (gameState)
             {
-                switch (gameState)
-                {
-                    case GameStates.GAME:
-                        Sequence showMerger = DOTween.Sequence();
-                        //Swap game state
-                        gameState = GameStates.INVENTORY;
+                case GameStates.GAME:
+                    Sequence showMerger = DOTween.Sequence();
+                    //Swap game state
+                    gameState = GameStates.INVENTORY;
 
-                        showMerger.Append(inventory.darkener.GetComponent<Image>().DOFade(.8f, .3f));
-                        showMerger.Append(inventory.mergeElements.transform.DOScale(1.2f, .3f).SetEase(Ease.InOutSine));
+                    showMerger.Append(inventory.darkener.GetComponent<Image>().DOFade(.8f, .3f));
+                    showMerger.Append(inventory.mergeElements.transform.DOScale(1.2f, .3f).SetEase(Ease.InOutSine));
 
-                        showMerger.Play();
-                        
-                        //Pos and show cursor
-                        inventory.cursor.transform.SetParent(inventory.uiInventory.transform.Find("Slots").GetChild(inventory.cursorIndex).transform);
-                        inventory.cursor.GetComponent<RectTransform>().localPosition = Vector3.zero;
-                        inventory.cursor.SetActive(true);
-                        break;
-                    case GameStates.INVENTORY:
-                        //Darkener
-                        inventory.darkener.GetComponent<Image>().DOFade(0f, .2f);
-                        //hide Glass
-                        inventory.mergeElements.transform.DOScale(0,.2f).SetEase(Ease.InOutSine);
-                        //hide cursor
-                        inventory.cursor.SetActive(false);
-                        //Swap game state
-                        //Clear merger
-                        inventory.playerSelection.Clear();
-                        inventory.atomToMergeA.GetComponent<Image>().sprite = inventory.mergeElements.GetComponent<AtomsSprites>().blank;;
-                        inventory.atomToMergeB.GetComponent<Image>().sprite = inventory.mergeElements.GetComponent<AtomsSprites>().blank;;
-                        gameState = GameStates.GAME;
-                        break;
-                }
-
-                timeSinceLastClick = 0;
+                    showMerger.Play();
+                    
+                    //Pos and show cursor
+                    inventory.cursor.transform.SetParent(inventory.uiInventory.transform.Find("Slots").GetChild(inventory.cursorIndex).transform);
+                    inventory.cursor.GetComponent<RectTransform>().localPosition = Vector3.zero;
+                    inventory.cursor.SetActive(true);
+                    break;
+                case GameStates.INVENTORY:
+                    //Darkener
+                    inventory.darkener.GetComponent<Image>().DOFade(0f, .2f);
+                    //hide Glass
+                    inventory.mergeElements.transform.DOScale(0,.2f).SetEase(Ease.InOutSine);
+                    //hide cursor
+                    inventory.cursor.SetActive(false);
+                    //Swap game state
+                    //Clear merger
+                    inventory.playerSelection.Clear();
+                    inventory.atomToMergeA.GetComponent<Image>().sprite = inventory.mergeElements.GetComponent<AtomsSprites>().blank;;
+                    inventory.atomToMergeB.GetComponent<Image>().sprite = inventory.mergeElements.GetComponent<AtomsSprites>().blank;;
+                    gameState = GameStates.GAME;
+                    break;
             }
         }
     }
